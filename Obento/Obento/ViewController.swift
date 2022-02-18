1e384ccb869b141930c2b0d2907542514b7549f6
Alejandro <aaizel@ucm.es>
2022-02-18 19:11:24 +0100

    Proyecto de iOS

---
+++ Obento/Obento/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Obento
+//
+//  Created by Alejandro Aizel Boto on 18/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
