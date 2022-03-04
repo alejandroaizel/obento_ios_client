9313103c2620cffc678b9a216ca10da0474e8ffc
Alejandro <aaizel@ucm.es>
2022-03-04 01:28:05 +0100

    F #1: Added Launch Screen
    Added the Obento logo in svg and the launch screen fully functional and resizable to all apple devices.

--- Obento/Obento/ViewController.swift
+++ Obento/Obento/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var olaButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
+    @IBAction func EnviarOla(_ sender: Any) {
+        print("ola")
 
+        var request = URLRequest(url: URL(string: "URL_DE_MIG")!)
+        request.httpMethod = "GET"
+
+        let session = URLSession.shared
+        let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
+            print(response!)
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!) as! Dictionary<String, AnyObject>
+                print(json)
+            } catch {
+                print("error")
+            }
+        })
+
+        task.resume()
+    }
+    
 }
 
