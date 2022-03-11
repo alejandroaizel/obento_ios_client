f5192ffe6274a0090e181bec597165a0b55e0efd
Alejandro <aaizel@ucm.es>
2022-03-11 21:23:42 +0100

    F #6: Added all sections and clickable featured recipe
    Clicking the featured recipe will show the recipe view with all the information of the recipe

---
+++ Obento/Obento/Views/RecipeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RecipeViewController.swift
+//  Obento
+//
+//  Created by Alejandro Aizel Boto on 11/3/22.
+//
+
+import UIKit
+
+class RecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Obento/Obento/Views/TodayViewController.swift
+++ Obento/Obento/Views/TodayViewController.swift
@@ -8,10 +8,42 @@
 import UIKit
 
 /// The view controller for the today tab
-class TodayViewController: UIViewController {
+class TodayViewController: UIViewController, UIGestureRecognizerDelegate {
+    // Feature recipe
+    @IBOutlet weak var featuredRecipePuntuaction: UILabel!
+    @IBOutlet weak var featuredRecipeTitle: UILabel!
+    @IBOutlet weak var featuredRecipekcal: UILabel!
+    @IBOutlet weak var featuredRecipeTime: UILabel!
+    @IBOutlet weak var featuredRecipePrice: UILabel!
+    @IBOutlet weak var featuredRecipeImage: UIImageView!
+    @IBOutlet weak var featuredRecipeView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(clickView(_:)))
+        tapGesture.delegate = self
+        
+        featuredRecipeView.addGestureRecognizer(tapGesture)
+        
+        let exampleRecipe = Recipe(id: 0, userId: 0, name: "Sopa caliente de çorba", description: "", puntuaction: 4.7, kcal: 350, time: 40, price: 6.5, isLaunch: true, imagePath: "recipe_1", ingredients: [], steps: [])
+        
+        loadFeatureRecipe(exampleRecipe)
+    }
+    
+    func loadFeatureRecipe(_ recipe: Recipe) {
+        featuredRecipePuntuaction.text = String(recipe.puntuaction)
+        featuredRecipeTitle.text = recipe.name
+        featuredRecipekcal.text = String(recipe.kcal) + " kcal"
+        featuredRecipeTime.text = String(recipe.time) + " min"
+        featuredRecipePrice.text = String(recipe.price) + " €"
+        featuredRecipeImage.image = UIImage(named: recipe.imagePath) // FIXME: Change this
+    }
+    
+    @objc func clickView(_ sender: UIView) {
+        let controller = (storyboard?.instantiateViewController(withIdentifier: "RecipeViewController"))!
+        
+        present(controller, animated: true, completion: nil)
     }
 }
 
